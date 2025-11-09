4e83a2fd98e78a80648b29fb49da5347cfebcaff
Baskaran M <baskarandeveloper1423@gmail.com>
2025-11-09 18:06:09 +0530

    feat: initial implementation of the week 01 core modules

--- node_mastery/week_01_core/02_modules_system/app.js
+++ node_mastery/week_01_core/02_modules_system/app.js
@@ -2,3 +2,7 @@ const math = require('./math');
 
 // log the entire math module
 console.log(math);
+
+// use the add function from the math module
+const result = math.add(5,10);
+console.log('The sum is:', result);
